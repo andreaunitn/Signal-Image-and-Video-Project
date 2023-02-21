83d1fa466da7960b9c64cf287fb34f713974235e
andreaunitn <andrea.tomasoni-1@studenti.unitn.it>
2023-02-21 21:33:05 +0100

    fixed warnings for deprecated functions

--- reid/models/resnet.py
+++ reid/models/resnet.py
@@ -3,34 +3,46 @@ from __future__ import absolute_import
 from torch import nn
 from torch.nn import functional as F
 from torch.nn import init
-import torchvision
+from torchvision.models import resnet18,resnet34,resnet50,resnet101,resnet152
+from torchvision.models import ResNet18_Weights,ResNet34_Weights,ResNet50_Weights,ResNet101_Weights,ResNet152_Weights
 
 
 __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
            'resnet152']
 
-
 class ResNet(nn.Module):
     __factory = {
-        18: torchvision.models.resnet18,
-        34: torchvision.models.resnet34,
-        50: torchvision.models.resnet50,
-        101: torchvision.models.resnet101,
-        152: torchvision.models.resnet152,
+        18: resnet18,
+        34: resnet34,
+        50: resnet50,
+        101: resnet101,
+        152: resnet152,
     }
 
-    def __init__(self, depth, pretrained=True, cut_at_pooling=False,
+    def __init__(self, depth, weights=None, cut_at_pooling=False,
                  num_features=0, norm=False, dropout=0, num_classes=0):
+
+        if depth == 18:
+            weights = ResNet18_Weights.IMAGENET1K_V1
+        elif depth == 34:
+            weights = ResNet34_Weights.IMAGENET1K_V1
+        elif depth == 50:
+            weights = ResNet50_Weights.IMAGENET1K_V1
+        elif depth == 101:
+            weights = ResNet101_Weights.IMAGENET1K_V1
+        elif depth == 152:
+            weights = ResNet152_Weights.IMAGENET1K_V1
+
         super(ResNet, self).__init__()
 
         self.depth = depth
-        self.pretrained = pretrained
+        self.weights = weights
         self.cut_at_pooling = cut_at_pooling
 
         # Construct base (pretrained) resnet
         if depth not in ResNet.__factory:
             raise KeyError("Unsupported depth:", depth)
-        self.base = ResNet.__factory[depth](pretrained=pretrained)
+        self.base = ResNet.__factory[depth](weights=weights)
 
         if not self.cut_at_pooling:
             self.num_features = num_features
@@ -45,10 +57,10 @@ class ResNet(nn.Module):
             if self.has_embedding:
                 self.feat = nn.Linear(out_planes, self.num_features)
                 self.feat_bn = nn.BatchNorm1d(self.num_features)
-                init.kaiming_normal(self.feat.weight, mode='fan_out')
-                init.constant(self.feat.bias, 0)
-                init.constant(self.feat_bn.weight, 1)
-                init.constant(self.feat_bn.bias, 0)
+                init.kaiming_normal_(self.feat.weight, mode='fan_out')
+                init.constant_(self.feat.bias, 0)
+                init.constant_(self.feat_bn.weight, 1)
+                init.constant_(self.feat_bn.bias, 0)
             else:
                 # Change the num_features to CNN output channels
                 self.num_features = out_planes
@@ -56,10 +68,10 @@ class ResNet(nn.Module):
                 self.drop = nn.Dropout(self.dropout)
             if self.num_classes > 0:
                 self.classifier = nn.Linear(self.num_features, self.num_classes)
-                init.normal(self.classifier.weight, std=0.001)
-                init.constant(self.classifier.bias, 0)
+                init.normal_(self.classifier.weight, std=0.001)
+                init.constant_(self.classifier.bias, 0)
 
-        if not self.pretrained:
+        if self.weights is None:
             self.reset_params()
 
     def forward(self, x):
