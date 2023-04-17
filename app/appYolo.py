94d78351bf1e632e69d36d931f6cc3a427c818e8
andreaunitn <andrea.tomasoni-1@studenti.unitn.it>
2023-04-17 22:02:17 +0200

    appYolo now finding most similar image

--- app/appYolo.py
+++ app/appYolo.py
@@ -1,10 +1,39 @@
-
-import cv2
+from reid.feature_extraction.cnn import extract_cnn_feature
+from reid.dist_metric import DistanceMetric
+from reid.utils.data import transforms as T
+from reid.evaluators import Evaluator
+from reid import models
+from PIL import Image
+import os.path as osp
 import numpy as np
+import torch
+import cv2
+
+def cosine_similarity_torch(x1, x2):
+
+    # Compute the dot product between the input tensors
+    dot_product = torch.dot(x1, x2)
+    
+    # Compute the L2 norm of the input tensors along the last dimension
+    x1_norm = torch.norm(x1, p=2, dim=-1)
+    x2_norm = torch.norm(x2, p=2, dim=-1)
+    
+    # Compute the cosine similarity between the input tensors
+    cosine_sim = dot_product / (x1_norm * x2_norm)
+    
+    return cosine_sim
+
+def load_checkpoint(fpath):
+    if osp.isfile(fpath):
+        checkpoint = torch.load(fpath)
+        print("=> Loaded checkpoint '{}'".format(fpath))
+        return checkpoint
+    else:
+        raise ValueError("=> No checkpoint found at '{}'".format(fpath))
 
 # Load the YOLOv3 model and configuration files
-net = cv2.dnn.readNetFromDarknet('yolo/yolov3.cfg', 'yolo/yolov3.weights')
-#net = cv2.dnn.readNetFromDarknet('yolo/yolov3-tiny.cfg', 'yolo/yolov3-tiny.weights')
+#net = cv2.dnn.readNetFromDarknet('yolo/yolov3.cfg', 'yolo/yolov3.weights')
+net = cv2.dnn.readNetFromDarknet('yolo/yolov3-tiny.cfg', 'yolo/yolov3-tiny.weights')
 
 # Load the COCO class labels
 classes = []
@@ -20,6 +49,20 @@ cap = cv2.VideoCapture(0)
 
 # Image counter to be incremented every time an image is saved
 counter = 0
+
+# Loading the model
+model = models.create("resnet50", num_features=1024, dropout=0, num_classes=751, last_stride=1)
+checkpoint = load_checkpoint("model_best.pth.tar")
+model.load_state_dict(checkpoint['state_dict'])
+
+# Initializing evaluation metric
+metric = DistanceMetric(algorithm="euclidean")
+
+# Initializing evaluator
+evaluator = Evaluator(model)
+
+# List af all saved images features
+dataset_features = []
 
 # Loop over frames from the video stream
 while True:
@@ -57,6 +100,16 @@ while True:
     # Apply non-maximum suppression to remove overlapping boxes
     indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
 
+    # Define image transformation
+    height = 256
+    width = 128
+    normalizer = T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    test_transformer = T.Compose([
+        T.RectScale(height, width),
+        T.ToTensor(),
+        normalizer,
+    ])
+
     # Draw bounding boxes around the people and save cropped images
     if len(indices) > 0:
         for i in indices.flatten():
@@ -80,11 +133,39 @@ while True:
                 h = frame.shape[0] - y
             if w > 0 and h > 0:
                 crop_img = frame[y:y+h, x:x+w]
-                file_name = f"{label}_{i}_{counter}.jpg"
+                file_name = f"{label}_{counter}.jpg"
                 counter += 1
+                image_tensor = crop_img[:, :, ::-1]
+                image_tensor = Image.fromarray(image_tensor.astype('uint8'), 'RGB')
+                image_tensor = test_transformer(image_tensor)
+                image_tensor = image_tensor
+
+                if torch.backends.mps.is_available():
+                    model = model.to('mps')
+                else:
+                    model = model.cuda()
+
+                query_features = extract_cnn_feature(model, image_tensor.unsqueeze(0))
+
+                if counter > 5:
+
+                    # Convert the list of features to a tensor
+                    all_features = torch.stack(dataset_features)
+
+                    # Compute the cosine similarity between the query image's feature and all other images' features
+                    cos_sim = torch.nn.functional.cosine_similarity(query_features, all_features)
+
+                    # Get the index of the most similar image
+                    most_similar_index = torch.argmax(cos_sim)
+
+                    print("{label}_{most_similar_index}.jpg")
+                    closest_img = cv2.imread(f"{label}_{most_similar_index}.jpg")
+                    cv2.imshow('most_similar_image', closest_img)
+
+
+                dataset_features += query_features
                 cv2.imwrite(file_name, crop_img)
             
-
     # Show the output frame
     cv2.imshow('frame', frame_copy)
 
@@ -95,7 +176,8 @@ while True:
     if key == ord('q'):
         break
 
+print(dataset_features)
+
 # Release the video stream and close all windows
 cap.release()
 cv2.destroyAllWindows()
-
